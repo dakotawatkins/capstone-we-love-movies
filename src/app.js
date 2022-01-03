7cb872f87d0b04b95d3dc70a0854765f1691e2c8
Dakota Watkins <dakota.watkins007@gmail.com>
2022-01-03 15:15:25 -0800

    updated migrations and added cors and heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
+const cors = require("cors");
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
